368b8151ca260ed367efa7a8c87a932d967a0a9b
David Li <li.davidm96@gmail.com>
2026-09-17 16:07:12 +0900

    fix(go): skip 404'd tables in GetObjects

--- go/connection.go
+++ go/connection.go
@@ -181,9 +181,11 @@ func (c *connectionImpl) GetTablesForDBSchema(ctx context.Context, catalog strin
 
 		md, err := table.Metadata(ctx, bigquery.WithMetadataView(bigquery.BasicMetadataView))
 		if err != nil {
-			if apiErr, ok := errors.AsType[*googleapi.Error](err); ok && apiErr.Code == http.StatusForbidden {
+			if apiErr, ok := errors.AsType[*googleapi.Error](err); ok && apiErr.Code == http.StatusForbidden || apiErr.Code == http.StatusNotFound {
 				// "User does not have permission to access results of another user's job"
 				// No sense in erroring the entire list operation; just treat it as nonexistent
+
+				// Similarly a 404 probably just means a TOC/TOU error and we can proceed
 				continue
 			}
 			return nil, errToAdbcErr(adbc.StatusInternal, err, "get table metadata for %s.%s.%s", catalog, schema, table.TableID)
